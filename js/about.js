a0a1945d6fc88437aebe051582819158dd8e3740
Devsk001 <srik3713@gmail.com>
2023-07-26 17:24:06 +0530

    About Us Section 2 Added | index Footer Changed.

--- js/about.js
+++ js/about.js
@@ -1,3 +1,34 @@
 
 pushInStart( '.push-in' );
+
+// loader
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 5000;
+function fun_loader(valueDisplay){
+    valueDisplays.forEach((valueDisplay) => {
+        let startValue = 0;
+        let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+        let duration = Math.floor(interval / endValue);
+        let counter = setInterval(function () {
+          startValue += 1;
+          valueDisplay.textContent = startValue + "K+";
+          if (startValue == endValue) {
+            clearInterval(counter);
+          }
+        }, duration);
+      });
+}
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        fun_loader();
+        return;
+      }
+      
+    });
+  });
+observer.observe(document.querySelector(".num"));
+
+//  end of the Fun Fact Loader.
+
 		
