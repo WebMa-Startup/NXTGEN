ca982231005cd35092f87dc481bd02a9094532b2
Devsk001 <srik3713@gmail.com>
2023-07-28 17:17:12 +0530

    Some Changes Added Index & About | Bug Fixed

--- js/about.js
+++ js/about.js
@@ -23,13 +23,24 @@ function fun_loader(valueDisplay) {
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      fun_loader();
+      valueDisplays.forEach((valueDisplay) => {
+        let startValue = 0;
+        let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+        let duration = Math.floor(interval / endValue);
+        let counter = setInterval(function () {
+          startValue += 1;
+          valueDisplay.textContent = startValue + "K+";
+          if (startValue == endValue) {
+            clearInterval(counter);
+          }
+        }, duration);
+      });
       return;
     }
 
   });
 });
-observer.observe(document.querySelector(".num"));
+observer.observe(document.querySelector(".container-loader span"));
 
 //  end of the Fun Fact Loader.
 
