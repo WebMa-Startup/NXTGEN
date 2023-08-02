c44d1c1c9e74f0b9ec0a2e1571af032648f10dbe
siththarth <sevigounder1802@gmail.com>
2023-08-02 21:56:08 +0530

    contact us page added

--- js/contact.js
+++ js/contact.js
@@ -1,51 +1,18 @@
+const inputs = document.querySelectorAll(".input");
 
-
-pushInStart('.push-in');
-
-
-// loader
-let valueDisplays = document.querySelectorAll(".num");
-let interval = 5000;
-function fun_loader(valueDisplay) {
-  valueDisplays.forEach((valueDisplay) => {
-    let startValue = 0;
-    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
-    let duration = Math.floor(interval / endValue);
-    let counter = setInterval(function () {
-      startValue += 1;
-      valueDisplay.textContent = startValue + "K+";
-      if (startValue == endValue) {
-        clearInterval(counter);
-      }
-    }, duration);
-  });
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
 }
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      valueDisplays.forEach((valueDisplay) => {
-        let startValue = 0;
-        let endValue = parseInt(valueDisplay.getAttribute("data-val"));
-        let duration = Math.floor(interval / endValue);
-        let counter = setInterval(function () {
-          startValue += 1;
-          valueDisplay.textContent = startValue + "K+";
-          if (startValue == endValue) {
-            clearInterval(counter);
-          }
-        }, duration);
-      });
-      return;
-    }
 
-  });
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
 });
-observer.observe(document.querySelector(".container-loader span"));
-
-//  end of the Fun Fact Loader.
-
-// animation on scroll
-AOS.init();
-
-
-
